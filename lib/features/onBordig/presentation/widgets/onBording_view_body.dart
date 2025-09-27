56594fd661a67291ef79df3c4fe679bcf7a27e9b
Mohamed Shaban <pmohamedshabaan@gmail.com>
2025-09-27 13:58:07 +0300

    finsh onbording view

--- lib/features/onBordig/presentation/widgets/onBording_view_body.dart
+++ lib/features/onBordig/presentation/widgets/onBording_view_body.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:fruitapp/core/app_image_assets.dart';
+import 'package:fruitapp/core/constant.dart';
+import 'package:fruitapp/core/services/shared_prefrence_singlton.dart';
+import 'package:fruitapp/core/widgets/custom_button.dart';
+import 'package:fruitapp/features/auth/persetation/login_view.dart';
 import 'package:fruitapp/features/onBordig/data/onBording_model.dart';
+import 'package:fruitapp/features/onBordig/presentation/widgets/buid_smooth_page_indicator.dart';
 import 'package:fruitapp/features/onBordig/presentation/widgets/page_view_item.dart';
 import 'package:fruitapp/generated/l10n.dart';
+
 
 class OnbordingViewBody extends StatefulWidget {
   const OnbordingViewBody({super.key});
@@ -12,27 +18,76 @@ class OnbordingViewBody extends StatefulWidget {
 }
 
 class _OnbordingViewBodyState extends State<OnbordingViewBody> {
- late OnbordingModel onbordingModel;
-
+ late OnbordingModel onbordingModel1;
+  late OnbordingModel onbordingModel2;
+  late PageController pageController;
+   int  currentPage=0;
   @override
+  void initState() {
+    super.initState();
+    pageController=PageController();
+    pageController.addListener((){
+      setState(() {
+        currentPage=pageController.page!.round();
+      });
+
+    });
+  }
+@override
   void didChangeDependencies() {
-    // TODO: implement didChangeDependencies
     super.didChangeDependencies();
-      intializModel();
+    intializeModel();
+    
+  }
+  @override
+  void dispose() {
+    super.dispose();
+    pageController.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return PageView(
+  
+    return Column(
       children: [
-       PageViewItem(onbordingModel: onbordingModel)
+        Expanded(
+          child: PageView(
+            controller: pageController,
+            children: [
+             PageViewItem(onbordingModel: onbordingModel1, currentPage:currentPage, isVisiable: true ,),
+            PageViewItem(onbordingModel: onbordingModel2, currentPage: currentPage,isVisiable:  false,),
+            
+          
+            ],
+          ),
+        ),
+       
+   BuildSmoothIndicator(pageController: pageController, currentPage: currentPage,)      ,
+  SizedBox(height: 29,),
+   Padding(
+
+     padding: const EdgeInsets.symmetric(horizontal: Constant.khorzintalPaddig),
+     child: Visibility(
+      visible: currentPage!=0,
+      maintainSize: true,
+      maintainAnimation: true,
+      maintainState: true,
+      child: CustomButton( onPressed: () {
+
+        SharedPrefrenceSinglton.setBool(Constant.kisOnBordingViewSeen, true);
+        Navigator.pushReplacementNamed(context, LoginView.routName);}
+        ,text:S.of(context).startNowKeyword ),),
+   ),
+   SizedBox(height: 43,)
+
       ],
     );
+    
   }
   
-  void intializModel() {
-    onbordingModel=OnbordingModel(image: Assets.fruitBasketImage,
-     title: S.of(context).onBordigViewTitle
-     , backgroundImage: Assets.vectorImage);
+  void intializeModel() {
+    onbordingModel1=OnbordingModel(image: Assets.assetsImagesFruitBasket, title: S.of(context).onBordig1Title , backgroundImage: Assets.assetsImagesVector, subTitle:  S.of(context).onBordig1SubTitle);
+     onbordingModel2=OnbordingModel(image: Assets.assetsImagesPineappleCuate, title: S.of(context).onBordig2Title , backgroundImage: Assets.assetsImagesVector, subTitle:  S.of(context).onBordig2SubTitle);
   }
+  
 }
