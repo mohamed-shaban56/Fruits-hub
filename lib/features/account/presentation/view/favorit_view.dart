197c65999bbfd9ee0ab50d98b6fdf9179f903aa1
Mohamed Shaban <pmohamedshabaan@gmail.com>
2025-11-05 23:38:33 +0200

    feat:Complete Favorit View

--- lib/features/account/presentation/view/favorit_view.dart
+++ lib/features/account/presentation/view/favorit_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:fruitapp/core/utliz/cubit/cart_cubit/cart_cubit.dart';
+import 'package:fruitapp/core/utliz/cubit/cart/cart_cubit/cart_cubit.dart';
+
 import 'package:fruitapp/core/utliz/widgets/build_app_bar.dart';
 import 'package:fruitapp/features/account/presentation/widgets/favorite_view_body.dart';
 import 'package:fruitapp/generated/l10n.dart';
