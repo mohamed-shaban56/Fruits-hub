aa75d8a68be8584ef7af5dff9d3ddf05f96df723
Mohamed Shaban <pmohamedshabaan@gmail.com>
2025-11-05 23:32:43 +0200

    style: update images, fonts, and colors

---
+++ lib/core/utliz/app_color.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+
+class AppColor {
+  static const primaryColor=Color(0xff1B5E37);
+   static const secondColor=Color(0xffFFB400);
+   static const lightSeconodColor=Color(0xffF8C76D);
+   static const linkColor=Color(0xff2D9F5D);
+   static const notificationColor=Color(0xffEEF8ED);
+   static const activeNavBarItemColor=Color(0xffEEEEEE);
+   static const gryColor=Color(0xffF3F5F7);
+   static const backgroundLightGreen = Color(0xFFEBF9F1);
+    static const Color grayBlue = Color(0xFF949D9E);
+  
+}
---
+++ lib/core/utliz/app_style.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class AppStyle {
+  static TextStyle bold23(){
+return TextStyle(
+
+fontSize: 23.sp,
+fontWeight: FontWeight.w700
+);
+  }
+  static TextStyle bold19(){
+return TextStyle(
+
+fontSize: 19.sp,
+fontWeight: FontWeight.w700
+);
+  }
+   static TextStyle bold16(){
+return TextStyle(
+
+fontSize: 16.sp,
+fontWeight: FontWeight.w700,
+
+);
+  }
+  static TextStyle bold13(){
+return TextStyle(
+
+fontSize: 13.sp,
+fontWeight: FontWeight.w700,
+
+);
+  }
+
+    static TextStyle semiBold13(){
+return TextStyle(
+
+fontSize: 13.sp,
+fontWeight: FontWeight.w600
+);
+
+  }
+    static TextStyle semiBold11(){
+return TextStyle(
+
+fontSize: 11.sp,
+fontWeight: FontWeight.w600
+);
+
+  }
+  static TextStyle semiBold16(){
+return TextStyle(
+
+fontSize: 16.sp,
+fontWeight: FontWeight.w600
+);
+
+  }
+
+    static TextStyle regular13(){
+return TextStyle(
+
+fontSize: 13.sp,
+fontWeight: FontWeight.w400
+);
+
+  }
+      static TextStyle regular16(){
+return TextStyle(
+color: Color(0xff949D9E),
+fontSize: 13.sp,
+fontWeight: FontWeight.w400
+);
+
+  }
+
+
+}
