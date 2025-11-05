e436b42e45c09362360445da784f6e1f7d6b3453
Mohamed Shaban <pmohamedshabaan@gmail.com>
2025-11-05 23:36:29 +0200

    feat:Complete Hive Configeration

---
+++ lib/core/utliz/services/product_model_adaptor.dart
@@ -0,0 +1,62 @@
+import 'package:fruitapp/features/dashbord/data/model/product_model.dart';
+import 'package:hive/hive.dart';
+
+class ProductModelAdapter extends TypeAdapter<ProductModel> {
+  @override
+  final int typeId = 1;
+
+  @override
+  ProductModel read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{};
+
+    for (int i = 0; i < numOfFields; i++) {
+      fields[reader.readByte()] = reader.read();
+    }
+
+    return ProductModel(
+      sellingCount: fields[0] as int,
+      productPrice: fields[1] as double,
+      productName: fields[2] as String,
+      productCode: fields[3] as String,
+      productDesc: fields[4] as String,
+      imageUrl: fields[5] as String,
+      experationMonths: fields[6] as int,
+      numOfcolories: fields[7] as int,
+      isOrganic: fields[8] as bool,
+      averageRating: fields[9] as num,
+      averageCount: fields[10] as num,
+      unit: fields[11] as int,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, ProductModel obj) {
+    writer
+      ..writeByte(12)
+      ..writeByte(0)
+      ..write(obj.sellingCount)
+      ..writeByte(1)
+      ..write(obj.productPrice)
+      ..writeByte(2)
+      ..write(obj.productName)
+      ..writeByte(3)
+      ..write(obj.productCode)
+      ..writeByte(4)
+      ..write(obj.productDesc)
+      ..writeByte(5)
+      ..write(obj.imageUrl)
+      ..writeByte(6)
+      ..write(obj.experationMonths)
+      ..writeByte(7)
+      ..write(obj.numOfcolories)
+      ..writeByte(8)
+      ..write(obj.isOrganic)
+      ..writeByte(9)
+      ..write(obj.averageRating)
+      ..writeByte(10)
+      ..write(obj.averageCount)
+      ..writeByte(11)
+      ..write(obj.unit);
+  }
+}
---
+++ lib/features/dashbord/data/model/product_model.dart
@@ -0,0 +1,90 @@
+
+
+import 'package:fruitapp/core/utliz/constant.dart';
+import 'package:fruitapp/features/dashbord/domain/entites/product_entity.dart';
+
+import 'package:hive/hive.dart';
+
+@HiveType(typeId: 1)
+class ProductModel extends HiveObject{
+ @HiveField(0)
+  final int sellingCount;
+  @HiveField(1)
+  final double productPrice;
+  @HiveField(2)
+final String productName;
+@HiveField(3)
+final String productCode;
+@HiveField(4)
+final String productDesc;
+@HiveField(5)
+ String imageUrl;
+ @HiveField(6)
+final int experationMonths;
+@HiveField(7)
+final int numOfcolories;
+@HiveField(8)
+final bool isOrganic;
+@HiveField(9)
+final num averageRating;
+@HiveField(10)
+final num averageCount;
+@HiveField(11)
+final int unit;
+ProductModel({required  this.experationMonths,required this.numOfcolories,required this.isOrganic,required this.averageRating,required this.averageCount,required this.unit,required this.productPrice, required this.productName, required this.productCode, required this.productDesc,required this.imageUrl,this.sellingCount=0  });
+
+  ProductEntity toEntity() {
+    return ProductEntity(
+      experationMonths: experationMonths,
+      numOfcolories: numOfcolories,
+      isOrganic: isOrganic,
+      averageRating: averageRating,
+      averageCount: averageCount,
+      unit: unit,
+      productPrice: productPrice,
+      productName: productName,
+      productCode: productCode,
+      productDesc: productDesc,
+      imageUrl: imageUrl,
+    );
+  }
+
+
+factory ProductModel.fromjson(Map<String,dynamic>json)
+{
+  return ProductModel(
+    experationMonths: json[Constant.kexperationManth], 
+  numOfcolories: json[Constant.knumberOfColories], 
+  isOrganic: json[Constant.kisOrganic],
+   averageRating: json[Constant.kaverageRating], 
+   averageCount: json[Constant.kratingCount],
+    unit: json[Constant.kunit], 
+    productPrice: (json[Constant.kproductPice]is String)?double.parse(json[Constant.kproductPice]):json[Constant.kproductPice],
+     productName: json[Constant.kproductName],
+      productCode: json[Constant.kproductCode],
+       productDesc: json[Constant.kproductDesc],
+        imageUrl: json[Constant.kImageUrl],
+        );
+}
+factory ProductModel.fromEntity(ProductEntity productEntity)
+{
+  return ProductModel(
+    productPrice: productEntity.productPrice, 
+    productName: productEntity.productName,
+     productCode: productEntity.productCode, 
+     productDesc: productEntity.productDesc,
+     imageUrl: productEntity.imageUrl,
+     
+       experationMonths: productEntity.experationMonths,
+       numOfcolories: productEntity.numOfcolories,
+        isOrganic: productEntity.isOrganic,
+         averageRating: productEntity.averageRating, 
+         averageCount: productEntity.averageCount,
+          unit: productEntity.unit,
+      
+      );
+}
+
+
+
+}
