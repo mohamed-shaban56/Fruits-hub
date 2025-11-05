83f0d3fdc853e7d1cfbbc55f560543ed26ae81ea
Mohamed Shaban <pmohamedshabaan@gmail.com>
2025-11-05 22:43:34 +0200

    refactor:Update CustomTextFild

--- lib/core/utliz/custom_text_fild.dart
+++ lib/core/utliz/custom_text_fild.dart
@@ -1,22 +1,36 @@
 import 'package:flutter/material.dart';
-import 'package:fruitapp/core/app_style.dart';
-import 'package:fruitapp/core/function_hulper/is_arabic.dart';
+import 'package:fruitapp/core/utliz/app_style.dart';
+import 'package:fruitapp/core/utliz/function_hulper/is_arabic.dart';
+import 'package:fruitapp/generated/l10n.dart';
 
 class CustomTextFild extends StatefulWidget {
-  const CustomTextFild({super.key, required this.hintText, required this.icon, required this.textInputType,this.ispassword=false});
+  const CustomTextFild({super.key,this.maxLines, this.controller,this.onChanged,  required this.hintText,  this.icon, required this.textInputType,this.ispassword=false});
 final String hintText;
-final IconData icon;
+final IconData? icon;
 final TextInputType textInputType;
 final bool ispassword;
+final int? maxLines;
+final TextEditingController? controller;
+final Function(String)? onChanged;
   @override
   State<CustomTextFild> createState() => _CustomTextFildState();
 }
-bool obSecretText=true;
+
 class _CustomTextFildState extends State<CustomTextFild> {
+bool obSecretText=true;
   @override
   Widget build(BuildContext context) {
     return TextFormField(
-
+      validator: (value) {
+        if(value!.isEmpty )
+        {
+          return "${widget.hintText } ${S.current.required_filed}";
+        }
+        return null;
+      },
+      maxLines:widget. maxLines ,
+      onChanged: widget.onChanged,
+controller:widget.controller ,
       keyboardType:  widget.textInputType ,
       obscureText:widget.ispassword?obSecretText: false,
       obscuringCharacter: '*',
@@ -34,11 +48,14 @@ class _CustomTextFildState extends State<CustomTextFild> {
         hintText: widget.hintText,
         hintStyle: AppStyle.bold13().copyWith(color: Color(0xff949D9E)),
         hintTextDirection: isArabic(context)?TextDirection.rtl:TextDirection.ltr,
-        suffixIcon:widget.ispassword?IconButton(onPressed: (){
+        suffixIcon:widget.ispassword?
+        IconButton(onPressed: (){
           setState(() {
             obSecretText=!obSecretText;
           });
-        }, icon: Icon(obSecretText? Icons.visibility_off :Icons.visibility ,color: Color(0xff949D9E),)):Icon(widget.icon,color:Color(0xff949D9E) ,)
+        }, icon: Icon(obSecretText? Icons.visibility_off :Icons.visibility ,color: Color(0xff949D9E),))
+        
+        :Icon(widget.icon,color:Color(0xff949D9E) ,)
         
         
       ),
