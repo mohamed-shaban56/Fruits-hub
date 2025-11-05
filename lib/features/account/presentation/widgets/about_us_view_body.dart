a1f2abc00046a28d2714e94493239c2d9150997b
Mohamed Shaban <pmohamedshabaan@gmail.com>
2025-11-05 23:41:41 +0200

    feat:Complete About Us View

--- lib/features/account/presentation/widgets/about_us_view_body.dart
+++ lib/features/account/presentation/widgets/about_us_view_body.dart
@@ -15,31 +15,37 @@ class AboutUsViewBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(18),
-      child: Container(
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(4)
-        ),
-        child: Padding(
-          padding: const EdgeInsets.all(18),
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                Text(S.current.about_us_section1,style: AppStyle.regular13().copyWith(color: AppColor.grayBlue), ),
-                SizedBox(height: 5.h,),
-                  Text(S.current.about_us_section2,style: AppStyle.regular13(), ),
-                SizedBox(height: 5.h,),
-                  Text(S.current.about_us_section3,style: AppStyle.regular13().copyWith(color: AppColor.grayBlue), ),
-                SizedBox(height: 5.h,),
-                  Text(S.current.about_us_section4,style: AppStyle.regular13() ),
-                SizedBox(height: 5.h,),
-                  Text(S.current.about_us_section5,style: AppStyle.regular13() ),
-                SizedBox(height: 5.h,),
-                Text(S.current.about_us_section6,style: AppStyle.regular13() ),
-                SizedBox(height: 5.h,),
-               
-               
-              ],
+      child: SingleChildScrollView(
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(4)
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(18),
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  Text(S.current.about_us_section1,style: AppStyle.regular13().copyWith(color: AppColor.grayBlue), ),
+                  SizedBox(height: 5.h,),
+                    Text(S.current.about_us_section2,style: AppStyle.regular13(), ),
+                  SizedBox(height: 5.h,),
+                    Text(S.current.about_us_section3,style: AppStyle.regular13().copyWith(color: AppColor.grayBlue), ),
+                  SizedBox(height: 5.h,),
+                    Text(S.current.about_us_section4,style: AppStyle.regular13() ),
+                  SizedBox(height: 5.h,),
+                    Text(S.current.about_us_section5,style: AppStyle.regular13() ),
+                  SizedBox(height: 5.h,),
+                  Text(S.current.about_us_section6,style: AppStyle.regular13() ),
+                  SizedBox(height: 5.h,),
+                   Text(S.current.about_us_section7,style: AppStyle.regular13() ),
+                  SizedBox(height: 5.h,),
+                   Text(S.current.about_us_section8,style: AppStyle.regular13() ),
+                  SizedBox(height: 5.h,),
+                 
+                 
+                ],
+              ),
             ),
           ),
         ),
