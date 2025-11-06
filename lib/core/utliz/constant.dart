72564b47b542b32ce635d902de1615464d47e4ff
Mohamed Shaban <pmohamedshabaan@gmail.com>
2025-11-06 16:12:04 +0200

    feat : add constant file

---
+++ lib/core/utliz/constant.dart
@@ -0,0 +1,31 @@
+class Constant{
+  static const khorzintalPaddig=16.0;
+   static const kpath='images';
+   static const kprofileImage='image';
+  static const kisOnBordingViewSeen="isOnBordingviewSeen";
+  static const kserverClientId="324216829781-e1obond80gq2mo5foq444ai8ir46v8nn.apps.googleusercontent.com";
+  static const  konBordingKey='onBordingKey';
+  static const khiveBoxName="favoriteProduct";
+  static const ksupabasebucket='avatar';
+    static const kfavoriteIProudct='favorite';
+static const klongInKey='loginKey';
+static const ksaveUserData='userDataKey';
+static const kproductPice='productPrice';
+static const kproductName='productName';
+  static const kproductDesc='productDesc';
+static const kproductCode='productCode';
+static const kstripe='stripe';
+static const kpaymob='paymob';
+static const kpaypal='paybal';
+static const kImageUrl='ImageUrl';
+static const kImageFile='ImageFile';
+static const kexperationManth='experationManth';
+static const knumberOfColories='numberOfColories';
+static const kratingCount='ratingCount';
+static const kaverageRating='averageRating';
+ static const kunit='unit';
+ static const kisOrganic='isOrganic';
+ static const ksubapaseUrl='https://mkwjyzdcjcboheejhrsl.supabase.co';
+
+static const ksubapaseKey='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1rd2p5emRjamNib2hlZWpocnNsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA0MzQ4MjAsImV4cCI6MjA3NjAxMDgyMH0.R23dkomLL278VME5_as9YLmXtqgbCjsOswxRNpnaXyc';
+}
