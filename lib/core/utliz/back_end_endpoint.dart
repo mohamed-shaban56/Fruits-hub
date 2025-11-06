8e899f140df8b9dbb7a433e6a2d07b780ee127a3
Mohamed Shaban <pmohamedshabaan@gmail.com>
2025-11-06 16:25:31 +0200

    chore: add Firebase paths constants file

---
+++ lib/core/utliz/back_end_endpoint.dart
@@ -0,0 +1,7 @@
+abstract class BackEndEndpoint {
+static const path="users";
+static const getUserData="users";
+static const productPath="product";
+static const orderPath="orders";
+static const updateOrder="orders";
+}
