60c4c9e9f5e107a8c90a56481c36ceebea4035c4
Mohamed Shaban <pmohamedshabaan@gmail.com>
2025-11-06 15:26:19 +0200

    feat : add firebase service (Firestore, Auth)

---
+++ lib/core/utliz/services/firebase_service.dart
@@ -0,0 +1,208 @@
+
+import 'dart:developer';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
+import 'package:fruitapp/core/utliz/constant.dart';
+import 'package:fruitapp/core/utliz/services/auth_service.dart';
+import 'package:fruitapp/core/utliz/back_end_endpoint.dart';
+import 'package:fruitapp/core/utliz/widgets/auth_exception.dart';
+import 'package:fruitapp/features/auth/domain/entity/update_user_data_entity.dart';
+
+import 'package:fruitapp/generated/l10n.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+
+class FirebaseService implements AuthService {
+final FirebaseAuth auth=FirebaseAuth.instance;
+
+FirebaseService();
+  @override
+  Future<User?> createUserWithEmailAndPassword({ required String email,required String password})async{
+try {
+  final credential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
+    email: email,
+    password: password,
+  );
+  return credential.user;
+} on FirebaseAuthException catch (e) {
+  if (e.code == 'weak-password') {
+        throw AuthException(S.current.weakPassword)    ;        
+  } else if (e.code == 'email-already-in-use') {
+    throw AuthException(S.current.emailAlreadyInUse)    ;        
+  }else if (e.code =="network-request-failed")
+  {
+    throw AuthException(S.current.noInternet);
+  }
+  
+  else{
+     log("Exception in FirebaseServices.createUserWithEmailAndPassword code ${e.code}");
+            throw AuthException(S.current.unexpectedError)    ;   
+  }
+} catch (e) {
+  log("Exception in FirebaseServices.createUserWithEmailAndPassword $e");
+ throw AuthException(S.current.unexpectedError)    ;   
+}
+  }
+
+
+
+  @override
+  Future<User?> signInUserWithEmailAndPassword({required String email, required String password})async
+  {
+    try {
+  final credential = await FirebaseAuth.instance.signInWithEmailAndPassword(
+    email: email,
+    password: password
+  );
+  return credential.user;
+} on FirebaseAuthException catch (e) {
+  if (e.code == 'user-not-found') {
+   throw AuthException(S.current.invalidCredentials);
+  } else if (e.code == 'wrong-password') {
+      throw AuthException(S.current.invalidCredentials);
+  }else if (e.code =="network-request-failed"){
+     throw AuthException(S.current.noInternet);
+  }else{
+    throw AuthException(S.current.unexpectedError);
+  }
+}catch (e)
+{
+  throw AuthException(S.current.unexpectedError);
+}
+  }
+
+@override
+  Future<User?> signInWithGoogle() async {
+
+
+try{
+// تهيئة GoogleSignIn إذا لم تفعلها
+ 
+  await GoogleSignIn.instance.initialize(
+    serverClientId: Constant.kserverClientId
+  );
+
+  // طلب تسجيل الدخول (authentication)
+  final GoogleSignInAccount account = await GoogleSignIn.instance.authenticate();
+
+final GoogleSignInAuthentication  googleAuth =  account.authentication;
+
+ final credential = GoogleAuthProvider.credential(
+   
+    idToken: googleAuth.idToken,
+  );
+    UserCredential userCredential= await FirebaseAuth.instance.signInWithCredential(credential);
+return userCredential.user;
+ 
+
+}on GoogleSignInException   catch (e)
+{
+  if(e.code==GoogleSignInExceptionCode.canceled)
+  {
+  log('user cancel login with goole $e');
+  return null;
+  }
+} catch(e)
+{
+  log('user cancel login with goole in general catch ${e.toString()}');
+  
+}
+return null;
+
+}
+
+ @override
+  Future<User?> signInWithGitHub() async {
+    try {
+      // إنشاء GitHub Provider
+      final githubProvider = GithubAuthProvider();
+      // تسجيل الدخول
+      final UserCredential userCredential =
+          await auth.signInWithProvider(githubProvider);
+
+      return userCredential.user;
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'account-exists-with-different-credential') {
+        log("الحساب مربوط بمزود آخر");
+      } else {
+        log(" FirebaseAuthException: ${e.message}");
+      }
+      return null;
+    } catch (e) {
+      log(" Unexpected error: $e");
+      return null;
+    }
+  }
+
+  @override
+  Future<User?> signInWithFacebook() async {
+  // Trigger the sign-in flow
+  final LoginResult loginResult = await FacebookAuth.instance.login();
+
+  // Create a credential from the access token
+  final OAuthCredential facebookAuthCredential = FacebookAuthProvider.credential(loginResult.accessToken!.tokenString);
+
+  // Once signed in, return the UserCredential
+  return  (await FirebaseAuth.instance.signInWithCredential(facebookAuthCredential)).user;
+}
+
+  @override
+  Future<void> deleteUser() async{
+  var users = FirebaseFirestore.instance.collection(BackEndEndpoint.path);
+  var uid=auth.currentUser?.uid;
+
+await users.doc(uid).delete();
+
+ 
+  }
+  
+  @override
+  Future<void> userSingOut() async{
+
+await FirebaseAuth.instance.signOut();
+ log("user singn out ");
+  await GoogleSignIn.instance.signOut();
+  log("user singn out from google ");
+  
+  
+  
+ log("user singn out from facebook ");
+
+
+ 
+  }
+  
+  @override
+ Future<void> updateEmailAndPassword({required UpdateUserDataEntity updateUserData}) async{
+   var user= FirebaseAuth.instance.currentUser;
+
+ try{
+
+  if(user !=null )
+ 
+  {
+final credential=EmailAuthProvider.credential(email: user.email!, password: updateUserData.currentPassword);
+  await user.reauthenticateWithCredential(credential);
+   await user.verifyBeforeUpdateEmail(updateUserData.newEmail);
+   await user.updatePassword(updateUserData.newPassword);
+   await user.updateDisplayName(updateUserData.newName);
+   if(user.emailVerified)
+   {
+ await userSingOut();
+   }
+ 
+  }
+  else{
+    log('لا يوجد حساب للمستخدم ');
+  }
+ }on FirebaseAuthException catch(e)
+ {
+  log('خطأ في المصادقة: ${e.code} - ${e.message}');
+ }
+  }
+  
+  
+
+}
---
+++ lib/core/utliz/services/firestore_service.dart
@@ -0,0 +1,101 @@
+
+
+
+
+import 'dart:developer';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:fruitapp/core/utliz/back_end_endpoint.dart';
+import 'package:fruitapp/core/utliz/services/data_base_service.dart';
+
+class FirestoreService implements DatabaseService{
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+  @override
+  Future<void> saveData(String path,Map<String,dynamic>data,String uid)async {
+try{
+  await firestore.collection(path).doc(uid).set(data);
+  log('add product to fire base');
+}
+catch(e)
+{
+  throw Exception('Fauild to add in FirestoreService $e');
+}
+  
+
+  }
+  
+  @override
+ Future<dynamic> getData({required String path,String? documentID, Map<String ,dynamic>? query}) async{
+  try{
+
+    if(documentID !=null)
+  {
+  var data = await FirebaseFirestore.instance.collection(path).doc(documentID).get();
+  return data.data();
+  }
+  else{
+     Query<Map<String, dynamic>> data =  FirebaseFirestore.instance.collection(path);
+if(query !=null)
+{
+  if(query['orderdBy'] !=null)
+{
+var orderBy=query['orderdBy'];
+var descending=query['decending'];
+   data=    data.orderBy(orderBy,descending: descending);
+}if(query['limit'] !=null)
+{
+  var limit=query['limit'];
+ data= data.limit(limit);
+}
+ 
+}
+ var result= await data.get();
+return result.docs.map((e)=>e.data()).toList();
+
+  }
+  }
+catch (e)
+{
+ throw Exception('Error while fetching data from Firebase: $e');
+}
+  
+
+  }
+  
+@override
+Future<void>deleteData(String path,String documentID)async{
+  try{
+await firestore.collection(path).doc(documentID).delete();
+log("User Deleted");
+  } catch (e)
+  {
+    log("Failed to delete user: $e");
+  throw Exception(e);
+  }
+
+}
+
+  @override
+  Stream <List<Map<String, dynamic>>> streamData({required String path}) {
+  final data =  FirebaseFirestore.instance.collection(path);
+   return    data.snapshots().map(
+    (snapshot)=>snapshot.docs.map((e)=>e.data()).toList()).handleError((error)=>log('error in stream function error is :$error'));
+
+
+
+  
+  }
+  
+  @override
+  Future updataData({required String path, String? documentID,required Map<String, dynamic> data})async {
+    try{
+  await  firestore.collection(BackEndEndpoint.updateOrder).doc(documentID).update(data);
+    }catch(e)
+    {
+throw Exception(e);
+    }
+
+  }
+  }
+
+ 
