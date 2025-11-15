8c8359907ef683e0a41d10ffdf81e92e386e8fe9
Mohamed Shaban <pmohamedshabaan@gmail.com>
2025-11-16 00:42:03 +0200

    refactor : update shopping button color

--- lib/features/home/presentation/widgets/shopping_button.dart
+++ lib/features/home/presentation/widgets/shopping_button.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:fruitapp/core/utliz/app_color.dart';
 import 'package:fruitapp/core/utliz/app_style.dart';
 
 class ShoppingButton extends StatelessWidget {
@@ -13,12 +14,12 @@ final  Function()? onTap;
       onTap:onTap ,
       child: Container(
         decoration: BoxDecoration(
-          color: Colors.white,
+          color: AppColor.offerItemTextColor(context),
           borderRadius: BorderRadius.circular(4)
         ),
         child: Padding(
-          padding:  EdgeInsets.symmetric(horizontal: 28.w,vertical: 4.h),
-          child: Text(buttonName,style: AppStyle.bold13(),),
+          padding:  EdgeInsets.symmetric(horizontal: 24.w,vertical: 8.h),
+          child: Text(buttonName,style: AppStyle.bold13().copyWith(color: AppColor.offerItemShopingTtext(context)),),
         ),
       ),
     );
