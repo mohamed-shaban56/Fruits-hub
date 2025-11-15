c9c7c6b2b414d80fb54d867c10831fe95ba5ff47
Mohamed Shaban <pmohamedshabaan@gmail.com>
2025-11-15 21:32:24 +0200

    refactor: update fruit item details

--- lib/features/home/presentation/widgets/fruit_item_details_back_button.dart
+++ lib/features/home/presentation/widgets/fruit_item_details_back_button.dart
@@ -16,14 +16,23 @@ class FruitItemDetailsBackButton extends StatelessWidget {
         
         Navigator.pop(context);
       },
-      child: Container(
-height: 44,
-width: 44,
-decoration: BoxDecoration(
-  color: Colors.white,
-  borderRadius: BorderRadius.circular(30),
-),
-child: Icon(Icons.arrow_back_ios,color: Colors.black,),
+      child: Stack(
+        children: [
+          Container(
+          height: 40,
+          width: 40,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(30),
+          ),
+          ),
+          Positioned(
+            top: 0,
+            bottom: 0,
+            right: 0,
+            left: -5,
+            child: Icon(Icons.arrow_back_ios,color: Colors.black,))
+        ],
       )
             
             
--- lib/features/home/presentation/widgets/fruit_item_details_product_info.dart
+++ lib/features/home/presentation/widgets/fruit_item_details_product_info.dart
@@ -1,14 +1,16 @@
 
 import 'package:flutter/material.dart';
 import 'package:fruitapp/core/app_image2.dart';
+import 'package:fruitapp/features/dashbord/domain/entites/product_entity.dart';
 import 'package:fruitapp/features/home/presentation/widgets/fruit_item_product_details_item.dart';
+import 'package:fruitapp/features/home/presentation/widgets/function_helper/get_average_rating.dart';
 import 'package:fruitapp/generated/l10n.dart';
 
 class FruitItemDetailsAllProductDetailsInfo extends StatelessWidget {
   const FruitItemDetailsAllProductDetailsInfo({
-    super.key,
+    super.key,required this.product
   });
-
+final ProductEntity product;
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -16,17 +18,17 @@ class FruitItemDetailsAllProductDetailsInfo extends StatelessWidget {
     
     Row(
       children: [
-    Expanded(child: FruitItemProductDetailsInfo(image: AppImage2.assetImageCalender, title: S.current.product_expiry, info: '',)),
+    Expanded(child: FruitItemProductDetailsInfo(image: AppImage2.assetImageCalender, title: S.current.product_expiry, info: "${product.experationMonths.toString()} ${S.current.Days}",)),
     SizedBox(width: 16,),
-    Expanded(child: FruitItemProductDetailsInfo(image: AppImage2.assetImageOrganic, title: S.current.product_category_organic, info: '',)),
+    Expanded(child: FruitItemProductDetailsInfo(image: AppImage2.assetImageOrganic, title: S.current.product_category_organic, info: '% ${product.organicPercentage}',)),
       ],
     ),
     SizedBox(height: 16,),
     Row(
       children: [
-    Expanded(child: FruitItemProductDetailsInfo(image: AppImage2.assetImageCalory, title: S.current.product_weight, info: '',)),
+    Expanded(child: FruitItemProductDetailsInfo(image: AppImage2.assetImageCalory, title: S.current.product_weight, info: '${product.numOfcolories} ${S.current.Calory}',)),
     SizedBox(width: 16,),
-    Expanded(child: FruitItemProductDetailsInfo(image: AppImage2.assetImageStar, title: S.current.review, info: '',)),
+    Expanded(child: FruitItemProductDetailsInfo(image: AppImage2.assetImageStar, title: S.current.review, info: getAverageRating(newValue: (product.productPrice/10).toInt()).toString(),)),
       ],
     ),
       ],
